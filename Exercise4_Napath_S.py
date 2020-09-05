3a0699310ee371e7e3ddeea5fd6379eab8dddcb6
GotItMyDADDY <maxmaanapath@gmail.com>
2020-09-05 18:33:42 +0700

    Add python file
    My Assignment
    --- Your Score ---
    Foundation English : คะแนนที่ได้
    General Business : คะแนนที่ได้
    Introduction to Computer Systems : คะแนนที่ได้
    Computer Programming : คะแนนที่ได้

---
+++ Exercise4_Napath_S.py
@@ -0,0 +1,10 @@
+FeScore = int(input("Score of Foundation English : "))
+GbScore = int(input("Score of General Business : "))
+IcScore = int(input("Score of Introduction to Computer Systems : "))
+CScore = int(input("Score of Computer Programming : "))
+
+print("--- Your Score ---")
+print("Foundation English :", FeScore)
+print("General Business :", GbScore)
+print("Introduction to Computer Systems :", IcScore)
+print("Computer Programming :", CScore)
