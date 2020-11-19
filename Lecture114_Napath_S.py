75fc5f3f86538f725d9e8ad34f0b2d9bc8f04156
GotItMyDADDY <maxmaanapath@gmail.com>
2020-11-20 02:46:46 +0700

    Add Lecture114
    Create the exchange rate calculator

---
+++ Lecture114_Napath_S.py
@@ -0,0 +1,45 @@
+from tkinter import *
+from tkinter import ttk
+from forex_python.converter import CurrencyRates, CurrencyCodes
+
+
+rate = CurrencyRates()
+code = CurrencyCodes()
+
+
+def Result(event):
+    result = (rate.convert(first_currency_box.get(), convert_currency_box.get(), float(amount.get())))
+    convert_result.configure(text = result)
+
+def NameRates(event):
+    name = code.get_currency_name(),
+
+main_window = Tk()
+title = Label(main_window, text = "Exchange rate calculator", bg = "black", font=("Saraban",10))
+title.grid(row = 0 , column = 1)
+first_currency = Label(main_window, text ="From : ")
+first_currency.grid(row = 1 , column = 0)
+first_currency_box = ttk.Combobox(main_window)
+first_currency_box['values'] = list(rate.get_rates("").keys())
+first_currency_box.current(29)
+first_currency_box.bind("<<ComboboxSelected>>")
+first_currency_box.grid(row = 1 , column = 1)
+text = Label(main_window, text = "Amounts")
+text.grid(row = 4 , column = 2)
+amount = Entry(main_window, text = "Input Amounts : ")
+amount.grid(row = 1 , column = 3)
+convert_currency = Label(main_window, text = "To : ")
+convert_currency.grid(row = 5 , column = 0)
+convert_currency_box = ttk.Combobox(main_window)
+convert_currency_box['values'] = list(rate.get_rates("").keys())
+convert_currency_box.current(10)
+convert_currency_box.bind("<<ComboboxSelected>>")
+convert_currency_box.grid(row = 5 , column = 1)
+convert_result = Label(main_window, text = "Result : ")
+convert_result.grid(row = 5 , column = 3)
+convert_button = Button(main_window, text = "Convert : ", width=15)
+convert_button.bind('<Button-1>', Result)
+convert_button.grid(row = 4 , column = 4)
+
+
+main_window.mainloop()
